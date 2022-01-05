f7dedb24a1abdc9586615ac858027668baee64d5
Dothuthao <1851161594@e.tlu.edu.vn>
2022-01-05 10:59:15 +0700

    Make a basic User model (including secure passwords)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  resources :static_pages
-  root 'application#hello'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'users/new'
+  scope "(:locale)", locale: /en|vi/ do
+    root "static_pages#home"
+    get "/home", to: "static_pages#home"
+    get "/help", to: "static_pages#help"
+    get "/contact", to: "static_pages#contact"
+    get "/about", to: "static_pages#about"
+    get "/signup", to: "users#new"
+  end
 end
